82e72264efa4450997e174402b3a400cf2d46e2c
Cristina <avicon95@protonmail.com>
2024-06-13 21:07:12 -0500

    feat: hero and brands sections

---
+++ app/components/Card.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+interface CardProps {
+    title: string;
+    url: string;
+    image: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+    title,
+    url,
+    image
+}) => {
+    return (
+        <div className="flex flex-col items-center hover:underline underline-offset-2">
+            <Image src={image} alt={title} width={300} height={200} />
+            <a href={url} className="flex flex-row space-x-2 mt-4">
+                <p className="text-2xl">{title}</p>
+                <Image src="/svgs/arrow-right.svg" alt="arrow-right" width={20} height={20} />
+            </a>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,26 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-pattern": "url('/images/hero-pattern.png')",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1600px",
+        "4xl": "1920px",
+        "5xl": "2560px",
+        "6xl": "3440px",
       },
     },
+    colors: {
+      "white": "#FFFFFF",
+      "black": "#000000",
+      "green": "#096F6D",
+      "yellow": "#EED37E"
+    }
   },
   plugins: [],
 };
